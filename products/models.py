6c7701c4fec68ccc1a14b212280144277aac70d7
Neoito <ifthikt@gmail.com>
2021-01-09 16:02:00 +0530

    Initial commit

---
+++ products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    item= models.CharField(max_length=200)
+    price= models.DecimalField(max_digits=10, decimal_places=2)
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return str(self.item) + ": $" + str(self.price)
